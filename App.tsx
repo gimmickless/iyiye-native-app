f7ad160c2a53ece4c767e90cf89179f4c6d29162
100117 <vahdetkeskin@gmail.com>
2020-08-24 15:11:46 +0300

    App.tsx change

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-// import 'react-native-gesture-handler'
-import { StatusBar } from 'expo-status-bar'
+import 'react-native-gesture-handler'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+// import { StatusBar } from 'expo-status-bar'
+// import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import HomeScreen from './src/screens/HomeScreen'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// })
 
 const Stack = createStackNavigator()
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { View } from 'react-native'
 
 const HomeScreen: React.FC = () => {
-  return <View></View>
+  return <View />
 }
 
 export default HomeScreen
