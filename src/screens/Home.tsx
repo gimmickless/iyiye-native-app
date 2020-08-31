4851d902104704d300f1a8b80743ed945267d4c6
100117 <vahdetkeskin@gmail.com>
2020-08-31 13:55:32 +0300

    add sample header customization for home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { View, StyleSheet, Button } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 const Home: React.FC = () => {
   const navigation = useNavigation()
+  // Customize header
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => <Button title="Home Header Button" onPress={() => {}} />
+    })
+  }, [navigation])
   console.log(navigation)
   return <View style={styles.view} />
 }
