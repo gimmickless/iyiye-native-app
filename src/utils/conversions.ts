babae4f9f3e9a57df5ac057781ad1cb8a823f5ee
vahdet <vahdetkeskin@gmail.com>
2021-04-01 23:47:48 +0300

    remove an unused import

--- src/utils/conversions.ts
+++ src/utils/conversions.ts
@@ -1,5 +1,3 @@
-import { AuthUserAddress } from 'types/context'
-
 export const convertDateToIsoString = (date: Date | undefined): string => {
   if (!date) return ''
   return date ? date.toISOString().substring(0, 10) : ''
